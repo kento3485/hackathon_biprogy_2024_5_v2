8efe6dfe826887d7600f28988c0c32e0945256c0
kento3485 <kento3485@gmail.com>
2024-05-18 23:58:10 +0900

    アプリの側っぽいのができた。

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
-const dotenv = require('dotenv');
-
-dotenv.config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.get('/liff-id', (req, res) => {
     res.json({ liffId: process.env.LIFF_ID });
+});
+
+app.post('/create-rental', (req, res) => {
+    // Logic to create rental
+    res.send('募集が作成されました');
+});
+
+app.get('/profile', (req, res) => {
+    // Logic to fetch profile
+    res.json({ name: "ビシェル ロロナ", rating: "★★★★☆", address: "〇〇県〇〇市〇〇町1-2-3" });
 });
 
 app.listen(port, () => {
