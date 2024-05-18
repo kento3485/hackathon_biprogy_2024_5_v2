bcf7cbf9bca5bc69566c8dffb977c45605ad3a47
kento3485 <kento3485@gmail.com>
2024-05-19 01:59:42 +0900

    募集一覧画面に他の画面へのボタンを貼った。

--- my-liff-app/src/components/RecruitmentList.js
+++ my-liff-app/src/components/RecruitmentList.js
@@ -1,10 +1,17 @@
 // src/components/RecruitmentList.js
 import React from 'react';
-import { Container, Typography, List, ListItem, ListItemText, Box } from '@mui/material';
+import { Container, Typography, List, ListItem, ListItemText, Box, BottomNavigation, BottomNavigationAction } from '@mui/material';
 import { rentals } from '../data/dummyData';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import GradeIcon from '@mui/icons-material/Grade';
 
 const RecruitmentList = () => {
+  const navigate = useNavigate();
+
   return (
     <Container>
       <Typography variant="h2" gutterBottom>募集一覧</Typography>
@@ -18,6 +25,20 @@ const RecruitmentList = () => {
           </ListItem>
         ))}
       </List>
+      <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+        <BottomNavigation
+          showLabels
+          onChange={(event, newValue) => {
+            navigate(newValue);
+          }}
+        >
+          <BottomNavigationAction label="募集作成" value="/create-recruitment" icon={<AddCircleIcon />} />
+          <BottomNavigationAction label="レンタル状況" value="/application-status" icon={<ListAltIcon />} />
+          <BottomNavigationAction label="検索設定" value="/search-settings" icon={<SearchIcon />} />
+          <BottomNavigationAction label="プロフィール" value="/user-profile" icon={<AccountCircleIcon />} />
+          <BottomNavigationAction label="評価" value="/review" icon={<GradeIcon />} />
+        </BottomNavigation>
+      </Box>
     </Container>
   );
 };
